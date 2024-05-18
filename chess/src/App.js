2fa807d478d4229598a1700d9b50ed5ae3ea7d93
winterkube <theicekubex@gmail.com>
2024-05-18 14:24:38 -0700

    bobby

--- chess/src/App.js
+++ chess/src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import Chessboard from './components/chessboard';
+import Start from './Start';
 
 function App() {
   return (
+
+    
+
     <div className="App">
       <header className="App-header">
+        <Start />
         <Chessboard />
       </header>
+
+
+      
     </div>
   );
 }
+
 
 export default App;
---
+++ chess/src/Start.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Start.css'; // Import CSS for styling
+
+const Start = () => {
+  return (
+    <div className="start">
+      <h1 className="title">Welcome to Our Hackathon Project</h1>
+      <p className="description">This is a brief description of our awesome AI project!</p>
+    </div>
+  );
+};
+
+export default Start;
