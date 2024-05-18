c62200b5818aa9093add6ab47e06ee727278935f
Nicholas Chan <nicholasch.an@outlook.com>
2024-05-18 15:04:20 -0700

    button

--- chess/src/components/Start.js
+++ chess/src/components/Start.js
@@ -6,6 +6,25 @@ const Start = () => {
     <div className="start">
       <h1 className="title">Welcome to Our Hackathon Project</h1>
       <p className="description">This is a brief description of our awesome AI project!</p>
+      <div className="button-container">
+      <button class="cssbuttons-io-button">
+        Get started
+        <div class="icon">
+          <svg
+            height="24"
+            width="24"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M0 0h24v24H0z" fill="none"></path>
+            <path
+              d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+              fill="currentColor"
+            ></path>
+          </svg>
+        </div>
+      </button>
+    </div>
     </div>
   );
 };
