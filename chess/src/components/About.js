0c496670f8396c0ba42d252d4538a2ed1610f871
Nicholas Chan <nicholasch.an@outlook.com>
2024-05-18 20:17:36 -0700

    banana

--- chess/src/components/About.js
+++ chess/src/components/About.js
@@ -8,10 +8,8 @@ const About = ({ isOpen, onClose }) => {
         <div className="about-overlay">
             <div className="about-content">
                 <h2>About Death By Chess</h2>
-                <h3> This is a game created by Jordan Mckenzie, <br/> Nick Chan, Bjorn Flamminman and Riley Su.</h3>
+                <h3> This is a game created by Jordan Mckenzie, <br/> Nick Chan, Bjorn Flaminman and Riley Su.</h3>
                 <button onClick={onClose}>Back</button>
-                <h1></h1>
-
             </div>
         </div>
     );
--- chess/src/components/PauseModal.js
+++ chess/src/components/PauseModal.js
@@ -9,14 +9,9 @@ const PauseModal = ({ isOpen, onClose, onExit, onReset, onAbout }) => {
             <div className="modal-content">
                 <h2>Game Paused</h2>
                 <button onClick={onClose}>Resume</button>
-                <h1></h1>
                 <button onClick={onReset}>Reset</button>
-                <h1></h1>
                 <button onClick={onAbout}>About</button>
-                <h1></h1>
                 <button onClick={onExit}>Exit</button>
-                <h1></h1>
-
             </div>
         </div>
     );
