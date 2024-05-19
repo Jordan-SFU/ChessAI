fc600bde24057ef07ac7be6b0bbb54e35e3a13ed
Jordan-SFU <jsm36@sfu.ca>
2024-05-18 20:14:23 -0700

    eee

--- chess.py
+++ chess.py
@@ -98,7 +98,7 @@ class chesspiece():
             match moveType:
                 case "capture":
                     #check if valid capture
-                    if target_piece.color == ("black" if self.color == "white" else "white"):
+                    if target_piece != 0 and target_piece.color == ("black" if self.color == "white" else "white"):
                         if self.check_path_clear(position):
                             print(f"{self.color} {self.name} captured {target_piece.color} {target_piece.name}")
                             if self.color == "black":
@@ -106,6 +106,7 @@ class chesspiece():
                             else:
                                 self.board.white_points += 1
                             self.move(position)
+                    return True
                             
                 case "attack":
                     if target_piece.color == ("black" if self.color == "white" else "white"):
@@ -116,6 +117,7 @@ class chesspiece():
                             else:
                                 self.board.white_points += 1
                             self.board.set_piece(0, self.position)
+                    return True
                 
                 case "swap":
                     if target_piece != 0:
@@ -123,12 +125,14 @@ class chesspiece():
                             prev_position = self.position
                             board.set_piece(self, position)
                             board.set_piece(target_piece, prev_position)
+                    return True
 
                 case "stun":
                     if target_piece != 0 and target_piece.color != self.color:
                         if self.check_path_clear(position):
                             print(f"{self.color} {self.name} stunned {target_piece.color} {target_piece.name}")
                             target_piece.stunned = True
+                    return True
 
                 case "push":
                     if target_piece != 0 and target_piece.color != self.color:
@@ -141,24 +145,27 @@ class chesspiece():
                             if 0 <= position[0] + delta_y < 8 and 0 <= position[1] + delta_x < 8 and self.board.get_piece([position[0] + delta_y, position[1] + delta_x]) == 0:
                                 target_piece.move([position[0] + delta_y, position[1] + delta_x])
                                 self.move(position)
+                    return True
 
                 case "shield":
                     if target_piece != 0 and target_piece.color == self.color:
                         if self.check_path_clear(position):
                             print(f"{self.color} {self.name} shielded {target_piece.color} {target_piece.name}")
                             target_piece.shielded = True
-                    pass
+                    return True
 
                 case "move":
                     if target_piece == 0:
                         if self.check_path_clear(position):
                             self.move(position)
+                    return True
 
                 case "first_move":
                     if board.get_turn_count() == 0:
                         if target_piece == 0:
                             if self.check_path_clear(position):
                                 self.move(position)
+                        return True
 
                 case _: 
                     print ("invalid move") 
@@ -325,33 +332,33 @@ print(board.get_board_state())
 
 current_color = "black"
 
-# while not board.game_over:
-#     board.display_board()
+while not board.game_over:
+    board.display_board()
 
-#     print("Current turn: ", current_color)
-#     print("Turn #: ", board.get_turn_count())
-#     print("Black points: ", board.black_points)
-#     print("White points: ", board.white_points)
-#     print("")
-#     piece_pos = input("Enter piece position (e.g., B2): ")
-#     piece_pos = board.convert_position(piece_pos)
+    print("Current turn: ", current_color)
+    print("Turn #: ", board.get_turn_count())
+    print("Black points: ", board.black_points)
+    print("White points: ", board.white_points)
+    print("")
+    piece_pos = input("Enter piece position (e.g., B2): ")
+    piece_pos = board.convert_position(piece_pos)
 
-#     piece = board.get_piece(piece_pos)
-#     # check if the piece is the correct color
-#     if piece == 0 or piece.color != current_color or piece.stunned == True:
-#         print("Invalid piece")
-#         continue
+    piece = board.get_piece(piece_pos)
+    # check if the piece is the correct color
+    if piece == 0 or piece.color != current_color or piece.stunned == True:
+        print("Invalid piece")
+        continue
 
-#     move_pos = input("Enter move position (e.g., C3): ")
-#     move_pos = board.convert_position(move_pos)
+    move_pos = input("Enter move position (e.g., C3): ")
+    move_pos = board.convert_position(move_pos)
 
-#     if piece.move(move_pos) == False:
-#         print("Invalid movement")
-#         continue
+    if piece.tileable(move_pos) == False:
+        print("Invalid movement")
+        continue
 
-#     if board.game_over:
-#         print(f"Game over! {board.winner.capitalize()} wins!")
-#         break
+    if board.game_over:
+        print(f"Game over! {board.winner.capitalize()} wins!")
+        break
 
-#     board.turn_count += 1
-#     current_color = "black" if current_color == "white" else "white"
+    board.turn_count += 1
+    current_color = "black" if current_color == "white" else "white"
