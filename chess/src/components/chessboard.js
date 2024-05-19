a92fca34fafc888d4e9ff1be8ad5a6f14abef9cc
Joodoon <jsm36@sfu.ca>
2024-05-19 01:20:48 -0700

    added highlights to start pos

--- chess/src/components/chessboard.js
+++ chess/src/components/chessboard.js
@@ -6,7 +6,6 @@ import PauseModal from './PauseModal';
 import About from './About';
 import { Chessboard } from "react-chessboard";
 
-
 function Menu() {
   const [isPaused, setIsPaused] = useState(false);
   const [isAbout, setIsAbout] = useState(false);
@@ -31,72 +30,83 @@ function Menu() {
     setIsPaused(false);
   };
 
-
   const handleExitClick = () => {
     navigate('/');
   };
-
 
   const handleStartGame = () => {
     setIsPieceSettingsOpen(false);
     // Implement the game start logic here
   };
 
+  const [moveSquares, setMoveSquares] = useState({});
+
   function onPieceDragBegin(piece, square) {
     console.log("onPieceDragBegin", piece, square);
+    highlightSquares([square]);
   }
 
   function onPieceDrop(piece, fromSquare, toSquare, dropResult) {
     console.log("onPieceDrop", piece, fromSquare, toSquare, dropResult);
+    // clear highlights
+    setMoveSquares({});
+  }
+
+  function highlightSquares(squares) {
+    const highlightStyle = { backgroundColor: 'rgba(255, 255, 0, 0.5)' };
+    const newMoveSquares = squares.reduce((acc, square) => {
+      acc[square] = highlightStyle;
+      return acc;
+    }, {});
+    setMoveSquares(newMoveSquares);
   }
 
   return (
-    
-      <div className="chessboard">
-        <div
-          style={{
-            width: "50vh",
-            height: "50vh",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Chessboard
-            id="Configurable Board"
-            onArrowsChange={function noRefCheck(){}}
-            onDragOverSquare={function noRefCheck(){}}
-            onMouseOutSquare={function noRefCheck(){}}
-            onMouseOverSquare={function noRefCheck(){}}
-            onPieceClick={function noRefCheck(){}}
-            onPieceDragBegin={onPieceDragBegin}
-            onPieceDragEnd={function noRefCheck(){}}
-            onPieceDrop={onPieceDrop}
-            onPromotionCheck={function noRefCheck(){}}
-            onPromotionPieceSelect={function noRefCheck(){}}
-            onSquareClick={function noRefCheck(){}}
-            onSquareRightClick={function noRefCheck(){}}
-          />
-        </div>
-        <button className="pause-button" onClick={handlePauseClick}>❚❚</button>
-        <PauseModal isOpen={isPaused}
-                    onClose={handleResumeClick}
-                    onReset={handleResetClick}
-                    onAbout={handleAboutClick}
-                    onExit={handleExitClick}
+    <div className="chessboard">
+      <div
+        style={{
+          width: "50vh",
+          height: "50vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Chessboard
+          id="Configurable Board"
+          onArrowsChange={function noRefCheck(){}}
+          onDragOverSquare={function noRefCheck(){}}
+          onMouseOutSquare={function noRefCheck(){}}
+          onMouseOverSquare={function noRefCheck(){}}
+          onPieceClick={function noRefCheck(){}}
+          onPieceDragBegin={onPieceDragBegin}
+          onPieceDragEnd={function noRefCheck(){}}
+          onPieceDrop={onPieceDrop}
+          onPromotionCheck={function noRefCheck(){}}
+          onPromotionPieceSelect={function noRefCheck(){}}
+          onSquareClick={function noRefCheck(){}}
+          onSquareRightClick={function noRefCheck(){}}
+          customSquareStyles={moveSquares}
         />
-        <PieceSettingsModal
-            isOpen={isPieceSettingsOpen}
-            onClose={() => setIsPieceSettingsOpen(false)}
-            onStart={handleStartGame}
-        />
-        <About
-            isOpen = {isAbout}
-            onClose={() =>setIsAbout(false)}
-          />
-        
       </div>
-      
+      <button className="pause-button" onClick={handlePauseClick}>❚❚</button>
+      <PauseModal
+        isOpen={isPaused}
+        onClose={handleResumeClick}
+        onReset={handleResetClick}
+        onAbout={handleAboutClick}
+        onExit={handleExitClick}
+      />
+      <PieceSettingsModal
+        isOpen={isPieceSettingsOpen}
+        onClose={() => setIsPieceSettingsOpen(false)}
+        onStart={handleStartGame}
+      />
+      <About
+        isOpen={isAbout}
+        onClose={() => setIsAbout(false)}
+      />
+    </div>
   );
 }
 
